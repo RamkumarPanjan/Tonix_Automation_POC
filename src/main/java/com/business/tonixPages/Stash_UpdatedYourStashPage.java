321ae01cd45d89effbbb0109b216e32c2d7a40dd
RamkumarPanjan <ramkumar.sivanandan@gmail.com>
2023-01-11 12:13:06 +0530

    Changes to verify and handle 'You updated your stash' confirmation page - Ramkumar

--- src/main/java/com/business/tonixPages/Stash_UpdatedYourStashPage.java
+++ src/main/java/com/business/tonixPages/Stash_UpdatedYourStashPage.java
@@ -12,28 +12,25 @@ public class Stash_UpdatedYourStashPage extends BasePage {
 	 * @throws Exception
 	 */
 
-	public void updatedYourStash() throws Exception 
-	{
-		extent.HeaderChildNode("You Updated Your Stash");
+	public void verifyUpdatedStashConfirmationMessage() throws Exception {
+		extent.HeaderChildNode("You Updated Your Stash page");
 		logger.info("After Modifying Stash Details Updated Stash Successfully Page ");
-
 		waitTime(2000);
-		if(verifyElementPresent(Stash_UpdateYourStashSelectors.txtYourUpdatedYourStash, "Updated Your Stash text"))
-		{
-			String actualMessageupdatedYourStash = getText(Stash_UpdateYourStashSelectors.txtYourUpdatedYourStash);
-			System.out.println(actualMessageupdatedYourStash);
-			verifyElementPresent(Stash_UpdateYourStashSelectors.txtThanksForUpdating, "Thanks For Updating text");
-			String actualMessageThanksForUpdating = getText(Stash_UpdateYourStashSelectors.txtThanksForUpdating);
-			System.out.println(actualMessageThanksForUpdating);
-		}
-		if(verifyElementPresent(Stash_UpdateYourStashSelectors.btnDone, "Done button is displayed"))
-		{
-			click(Stash_UpdateYourStashSelectors.btnDone, "Done button");
-			extent.extentLoggerPass("You Updated Your Stash", "Clicked on You Updated Your Stash in Updated stash  page");
-		}
-		else
-		{
-			extent.extentLoggerFail("You Updated Your Stash", "Unable to click on Done button in Updated stash  page");
+
+		if(waitForElementToBePresent(Stash_UpdateYourStashSelectors.txtYourUpdatedYourStash,120,"You updated your stash text")) {
+			String actualMessage = getText(Stash_UpdateYourStashSelectors.txtThanksForUpdating);
+			String expectedMessage = "\"Update\" when you go out with someone who is better looking than you. Thanks for updating.";
+			softAssertion.assertEquals(actualMessage, expectedMessage);
+			softAssertion.assertAll();
+			if(verifyElementPresent(Stash_UpdateYourStashSelectors.btnDone, "Done button is displayed")) {
+				click(Stash_UpdateYourStashSelectors.btnDone, "Done button");
+				extent.extentLoggerPass("You Updated Your Stash", "Clicked on You Updated Your Stash in Updated stash  page");
+			}
+			else {
+				extent.extentLoggerFail("You Updated Your Stash", "Unable to click on Done button in Updated stash  page");
+			}
+		} else {
+			extent.extentLoggerFail("You Updated Your Stash page", "Page not loaded successfully: 'You Updated Your Stash'");
 		}
 	}
 }
