980bef2abde6a5e83b6862f7430ab1e924480e91
Avula Harish <avulaharish1996@gmail.com>
2023-01-13 12:27:42 +0530

    Changes made in Base Page --harish

--- src/main/java/com/business/tonixPages/BasePage.java
+++ src/main/java/com/business/tonixPages/BasePage.java
@@ -3,6 +3,12 @@ package com.business.tonixPages;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.AssertJUnit.assertNotNull;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.testng.asserts.SoftAssert;
 
 import com.Datasheet.RingPay_TestData_DataProvider;
@@ -16,6 +22,8 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
 public class BasePage extends Utilities {
+	
+	public String formattedDate =null;
 
 	public BasePage() throws InterruptedException {
 		//new CommandBase(Application);
@@ -27,7 +35,7 @@ public class BasePage extends Utilities {
 		init();
 	}
 
-    
+
 	RingPay_TestData_DataProvider dataProvider = new RingPay_TestData_DataProvider();
 	public int timeout;
 	SoftAssert softAssertion = new SoftAssert();
@@ -84,5 +92,32 @@ public class BasePage extends Utilities {
 		logger.info("App tear Down");
 		getDriver().quit();
 	}
-	
+
+
+	public  String dateComparison() {
+		LocalDateTime myDateObj = LocalDateTime.now(ZoneId.of("UTC+08:00"));
+		System.out.println("Before formatting: " + myDateObj);
+		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd MMM YYYY hh.mm a");
+		formattedDate= myDateObj.format(myFormatObj);
+		System.out.println("After formatting: " + formattedDate);
+		ZoneId zone = ZoneId.of("UTC+08:00");
+		ZonedDateTime zdt = myDateObj.atZone(zone);
+		Instant instant = zdt.toInstant();
+		instant.toString();
+		return formattedDate;
+	}
+
+	public  String dateComparisonWithoutTime() {
+		LocalDateTime myDateObj = LocalDateTime.now(ZoneId.of("UTC+08:00"));
+		System.out.println("Before formatting: " + myDateObj);
+		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd MMM YYYY");
+		formattedDate= myDateObj.format(myFormatObj);
+		System.out.println("After formatting: " + formattedDate);
+		ZoneId zone = ZoneId.of("UTC+08:00");
+		ZonedDateTime zdt = myDateObj.atZone(zone);
+		Instant instant = zdt.toInstant();
+		instant.toString();
+		return formattedDate;
+	}
+
 }
